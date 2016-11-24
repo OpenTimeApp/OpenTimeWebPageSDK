0d1b8cce6132d390e8ff99c79db20cec3029279d
sergeome <sergeome@gmail.com>
2016-11-24 13:02:11 -0600

    Implemented example of request using plain XHR and AJAX

--- src/sdk/otwp-http.service.ts
+++ src/sdk/otwp-http.service.ts
@@ -1,34 +1,25 @@
 import { Injectable } from "@angular/core";
-import { Http, ConnectionBackend, RequestMethod, RequestOptions } from "@angular/http";
-import 'rxjs/Rx';
 
 @Injectable()
 
 export class OTWPHttpService {
 
-  constructor() {}
-
-  _connectionBackend = ConnectionBackend();
-  _options = new RequestOptions({
-    method: RequestMethod.Get,
-    url: 'http://opentimeapp.com/doctors.php?page=1&rows=5'
-  });
-
-  exampleUrl = "http://opentimeapp.com/doctors.php?page=1&rows=5";
-
-  http = new Http(this._connectionBackend, this._options);
-
-  //Promise example
-  getDataToPromise() {
-    return this.http.get(this.exampleUrl)
-      .map(response => response.json())
-      .toPromise()
+  constructor() {
   }
 
-  //Observable example
-  getDataAsObservable() {
-    return this.http.get(this.exampleUrl)
-      .map(response => response.json())
+  http = new XMLHttpRequest();
+
+  url = "http://opentimeapp.com/doctors.php?page=1&rows=5";
+  method = 'GET';
+
+  getDoctors(callback) {
+    this.http.open(this.method, this.url);
+    this.http.onreadystatechange = () => {
+      if (this.http.readyState === XMLHttpRequest.DONE) {
+        callback(JSON.parse(this.http.responseText));
+      }
+    };
+    this.http.send();
   }
 
 }
