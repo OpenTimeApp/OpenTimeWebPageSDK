9565872b2e6219d3706e547fb7213f2f2e24fb8a
Josh Woodcock <josh.woodcock@opentimeapp.com>
2016-11-25 15:27:11 -0600

    Removed jQuery

--- src/sdk/libraries/http-app-request.ts
+++ src/sdk/libraries/http-app-request.ts
@@ -1,27 +1,57 @@
-import * as $ from 'jquery';
-
 export class HttpAppRequest {
 
-    private _headers: Object;
+    public constructor() {}
 
-    public constructor() {
-        this._headers = {};
-    }
+    private _http = new XMLHttpRequest();
+    private _headers: Array<string> = [];
 
     public getResponse(url: string, method: string, data: any, callback: (responseObject) => void) {
 
-        $.ajax(url, {
-            method: method,
-            data: data,
-            cache: false,
-            headers: this._headers,
-            dataType: 'json'
-        }).done((response) => {
-            callback(response);
-        }).fail((jqXHR, errorText) => {
-            console.log(jqXHR.responseText);
-            callback({success: false, message: errorText, data: null});
-        });
+        let finalUrl = url;
+
+        // Convert data object into query string
+        if (method.toUpperCase() === 'GET' && !url.includes('?')) {
+            finalUrl += '?' + Object.keys(data).map(k => `${encodeURIComponent(k)}=${encodeURIComponent(data[k])}`).join('&');
+        }
+
+        let queryString = '';
+
+        this._http.open(method, finalUrl, true);
+
+        for (let key in this._headers) {
+            this._http.setRequestHeader(key, this._headers[key]);
+        }
+
+        if(method !== 'GET') {
+            queryString = Object.keys(data).map(k => `${encodeURIComponent(k)}=${encodeURIComponent(data[k])}`).join('&');
+
+            this._http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        }
+
+        this._http.onreadystatechange = () => {
+            if (this._http.readyState === XMLHttpRequest.DONE) {
+
+                try {
+                    let jsonData = JSON.parse(this._http.responseText);
+
+                    callback(jsonData);
+                } catch (e) {
+
+                    let errorMessage = {
+                        success: false,
+                        message: "The server did not return a valid JSON response",
+                        data: null
+                    };
+
+                    console.error("The server did not return a valid JSON response for url: " + url);
+                    console.error("Response: " + this._http.responseText);
+
+                    callback(errorMessage);
+                }
+            }
+        };
+
+        this._http.send(queryString);
     }
 
     public setRequestHeader(header: string, value: string) {
